5793c321b2c6544b818dfc3eb256b23538478743
Sohail Alakhdar <sohailalakhdar@gmail.com>
2026-09-23 17:08:49 +0300

    remove unused user model and index logging from database connection

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -6,29 +6,13 @@ async function connectDB() {
     await mongoose.connect(uri);
     console.log(`[DB] MongoDB connected: ${mongoose.connection.host}`);
 
-    // لازم تلاقي: firstName_1: [ [ 'firstName', 1 ] ]
-    const indexes = await User.collection.listIndexes().toArray();
-    console.log(indexes);
   } catch (err) {
     console.error('[DB] Connection error:', err.message);
     process.exit(1);
   }
 }
 
-const UserAchievement = require('./../models/UserAchievement.js'); // عدّل المسار
-const User = require('../models/User.js');
 
-async function activateAllSurahs() {
-  await mongoose.connect(process.env.MONGO_URI);
-
-  const result = await UserAchievement.updateMany(
-    {},
-    { $set: { isActive: true } }
-  );
-
-  console.log(`Matched: ${result.matchedCount}, Modified: ${result.modifiedCount}`);
-  await mongoose.disconnect();
-}
 
 // activateAllSurahs().catch(console.error);
 module.exports = connectDB;
